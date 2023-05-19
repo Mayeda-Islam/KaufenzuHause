e3d920ede2393091890018ffb61d575e15b587de
unknown <mayedakonika@gmail.com>
2023-05-19 14:22:32 +0600

    fix slider images

--- src/components/Home/CategorySection/CategorySectionOne.jsx
+++ src/components/Home/CategorySection/CategorySectionOne.jsx
@@ -1,8 +1,12 @@
 import products from "../../../Shared/FakeDataProducts";
 import ProductCard from "../../../Shared/ProductCard/ProductCard";
+import categoryImage1 from "../../../images/category section/Home_appliances_1.jpg";
+import categoryImage2 from "../../../images/category section/Home_Appliances_2_1.jpg";
 import CategorySlider from "../../../Shared/CategorySlider/CategorySlider";
 
 const CategorySectionOne = () => {
+  const sliderImages = [categoryImage1, categoryImage2];
+  console.log(sliderImages);
   return (
     <div className="shadow-md pb-14">
       <div className="mx-auto w-4/5">
@@ -11,7 +15,7 @@ const CategorySectionOne = () => {
         <hr className="my-4" />
         <div className="grid grid-cols-1 md:grid-rows-3 md:grid-cols-[464px_auto] gap-x-6 pb-5">
           <div className="row-span-3 hidden md:block ">
-            <CategorySlider></CategorySlider>
+            <CategorySlider sliderImages={sliderImages}></CategorySlider>
           </div>
           <div className="row-span-3">
             <div className=" grid grid-cols-2 md:grid-cols-3 gap-4 ">
--- src/components/Home/CategorySection/CategorySectionTwo.jsx
+++ src/components/Home/CategorySection/CategorySectionTwo.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import CategorySlider from "../../../Shared/CategorySlider/CategorySlider";
+// import CategorySlider from "../../../Shared/CategorySlider/CategorySlider";
+import categoryImage2 from "../../../images/category section/Gadget_1.jpg";
+import categoryImage1 from "../../../images/category section/Gadget_2.jpg";
+
 import ProductCard from "../../../Shared/ProductCard/ProductCard";
 import products from "../../../Shared/FakeDataProducts";
+import CategorySlider from "../../../Shared/CategorySlider/CategorySlider";
 
 const CategorySectionTwo = () => {
+  const sliderImages = [categoryImage1, categoryImage2];
   return (
     <div className="shadow-md pb-14">
       <div className="mx-auto w-4/5">
@@ -12,7 +17,7 @@ const CategorySectionTwo = () => {
         <hr className="my-6" />
         <div className="grid grid-cols-1 md:grid-rows-3 md:grid-cols-[464px_auto] gap-x-6 pb-5">
           <div className="row-span-3 hidden md:block ">
-            <CategorySlider></CategorySlider>
+            <CategorySlider sliderImages={sliderImages}></CategorySlider>
           </div>
           <div className="row-span-3">
             <div className=" grid grid-cols-2 md:grid-cols-3 gap-4 ">
