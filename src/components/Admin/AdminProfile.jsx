01c0441b7bd25ac15d3e87e91180c227e1f3ab7a
unknown <mayedakonika@gmail.com>
2023-05-26 17:03:42 +0600

    country api and validation added

--- src/components/Admin/AdminProfile.jsx
+++ src/components/Admin/AdminProfile.jsx
@@ -1,40 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { SingleImageUploader } from "../../APIHooks/SingleImageUploader";
 
 const AdminProfile = () => {
+  const [image, setImage] = useState(null);
+  const [countries, setCountries] = useState([]);
   const {
     register,
     handleSubmit,
-    watch,
+    // watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const handleUpdateProfile = (data) => console.log(data);
+  const handleImageUpload = async (event) => {
+    const imageData = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", imageData);
+
+    SingleImageUploader(formData, setImage);
+  };
+
+  // countries api
+  const uri = `https://restcountries.com/v3.1/all`;
+  fetch(uri)
+    .then((res) => res.json())
+    .then((data) => setCountries(data));
+
   return (
     <div>
       <div className="grid grid-cols-12 w-fit mx-auto md:gap-10">
         <div className="col-span-12 lg:col-span-4 flex justify-center">
           <div className="mt-12">
-            {/* {imageUrl ? (
-                <img
-                  className="rounded-full w-36 h-36 object-cover border-4 border-[#f5f8ff] mx-auto"
-                  src={imageUrl}
-                  alt="This is profile pic"
-                />
-              ) : ( */}
-            <img
-              className="rounded-full w-72 border-4 border-[#f5f8ff] mx-auto"
-              src={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXnHCfhPKKAy1zSl8__FmI1hsMmSR-MVgh5IcfD_-43Q&s"
-              }
-              alt="This is profile pic"
-            />
-            {/* )} */}
-            {/* <h2 className="text-lg font-medium mt-5">Admin</h2> */}
+            {image ? (
+              <img
+                className="rounded-full w-72 h-72 object-cover border-4 border-[#f5f8ff] mx-auto"
+                src={image}
+                alt="This is profile pic"
+              />
+            ) : (
+              <img
+                className="rounded-full w-72 border-4 border-[#f5f8ff] mx-auto"
+                src={
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXnHCfhPKKAy1zSl8__FmI1hsMmSR-MVgh5IcfD_-43Q&s"
+                }
+                alt="This is profile pic"
+              />
+            )}
+            {/* <h2 className="text-lg font-medium mt-5">Admin</h2>  */}
           </div>
         </div>
 
         <div className="col-span-12 mt-12  lg:col-span-8 w-fit lg:w-full mx-auto ">
-          <h2 className="text-4xl font-medium pt-5">Mayeda Islam </h2>
+          <h2 className="text-4xl font-medium pt-5">Profile </h2>
           <p className="text-xl text-blue-400 my-2">
             rayn@gmail.com{" "}
             <span className="text-xl text-gray-500">- Admin</span>
@@ -70,7 +87,13 @@ const AdminProfile = () => {
                   SVG, PNG, JPG or GIF (MAX. 800x400px)
                 </p>
               </div>
-              <input id="dropzone-file" type="file" className="hidden" />
+              <input
+                onChange={handleImageUpload}
+                id="dropzone-file"
+                accept="image/*"
+                type="file"
+                className="hidden"
+              />
             </label>
           </div>
         </div>
@@ -85,12 +108,15 @@ const AdminProfile = () => {
             </span>
             <input
               type="fullName"
-              {...register("email", { required: false })}
+              {...register("fullName", { required: true })}
               // defaultValue={user?.email}
-              disabled
+
               className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
-              placeholder="Mayeda Islam"
+              placeholder="your name"
             />
+            {errors?.fullName && (
+              <p className="text-red-500 text-sm ">Your name is required</p>
+            )}
           </label>
           <label className="block mt-4">
             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium">
@@ -98,28 +124,36 @@ const AdminProfile = () => {
             </span>
             <input
               type="email"
-              {...register("email", { required: false })}
+              {...register("email", { required: true })}
               // defaultValue={user?.email}
-              disabled
+
               className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
               placeholder="you@example.com"
             />
+            {errors?.email && (
+              <p className="text-red-500 text-sm ">Your email is required</p>
+            )}
           </label>
 
           <div className="grid grid-cols-2 gap-x-4 mt-4">
             <div>
-              <label className="block">
+              <label className="block ">
                 <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium">
                   Phone Number
                 </span>
                 <input
                   type="tel"
-                  {...register("number", { required: false })}
+                  {...register("number", { required: true })}
                   pattern="^(?:(?:\+|00)88|01)?\d{11}$"
                   // defaultValue={user?.number}
                   className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
                   placeholder="01934****39"
                 />
+                {errors?.email && (
+                  <p className="text-red-500 text-sm ">
+                    Your phone number is required
+                  </p>
+                )}
               </label>
             </div>
 
@@ -159,7 +193,7 @@ const AdminProfile = () => {
               </select>
             </div>
           </div>
-          <div className="block ">
+          <div className="block mt-4">
             <label
               // for="default"
               className="block mb-2 text-sm font-medium text-gray-900"
@@ -168,15 +202,19 @@ const AdminProfile = () => {
             </label>
             <input
               type="address"
-              {...register("email", { required: false })}
+              {...register("address", { required: true })}
               // defaultValue={user?.email}
               disabled
               className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
               placeholder="house:a,road:b,block:x"
             />
+            {errors?.address && (
+              <p className="text-red-500 text-sm ">Your address is required</p>
+            )}
           </div>
 
           <div className="grid grid-cols-2 gap-x-4 mt-4">
+            {/*********************  city input ****************/}
             <div>
               <label
                 // for="default"
@@ -184,54 +222,67 @@ const AdminProfile = () => {
               >
                 City
               </label>
-              <select
-                {...register("city", { required: false })}
-                // defaultValue={user?.city}
-                className="border-2 text-gray-900 mb-6 text-sm rounded-lg block w-full p-2.5 focus:outline-none border-[#55c3c1f7] bg-transparent"
-              >
-                <option selected hidden>
-                  Choose One
-                </option>
-                ;
-              </select>
+
+              <input
+                type="text"
+                {...register("city", { required: true })}
+                // defaultValue={user?.email}
+
+                className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
+                placeholder="example city"
+              />
+              {errors?.email && (
+                <p className="text-red-500 text-sm ">Your city is required</p>
+              )}
             </div>
-            <div>
-              <label
-                // for="default"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Country
-              </label>
-              <select
-                {...register("country", { required: false })}
-                // defaultValue={user?.country}
-                className="border-2 text-gray-900 mb-6 text-sm rounded-lg block w-full p-2.5 focus:outline-none border-[#55c3c1f7] bg-transparent"
-              >
-                <option selected hidden>
-                  Choose One
-                </option>
-                <option
-                  value="bangladesh"
-                  // selected={user?.country === "bangladesh"}
-                >
-                  Bangladesh
-                </option>
-              </select>
-            </div>
+            {/*********************  zip code input ****************/}
+            <label className="block">
+              <span className=" block text-sm font-medium">Zip Code</span>
+              <input
+                type="number"
+                {...register("zipCode", { required: true })}
+                // defaultValue={user?.zipCode}
+                className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
+                placeholder="Enter your zip code"
+              />
+              {errors?.zipCode && (
+                <p className="text-red-500 text-sm ">Zip code is required</p>
+              )}
+            </label>
           </div>
-          <label className="block">
-            <span className=" block text-sm font-medium">Zip Code</span>
-            <input
-              type="number"
-              {...register("zipCode", { required: false })}
-              // defaultValue={user?.zipCode}
-              className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
-              placeholder="Enter your zip code"
-            />
-          </label>
+          {/**************************  country  ***********************/}
+          {countries.map((country) => (
+            <></>
+          ))}
+          <div className="mt-4 block">
+            <label
+              // for="default"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Country
+            </label>
+            <select
+              {...register("country", { required: false })}
+              // defaultValue={user?.country}
+              className="border-2 text-black mb-6 text-sm rounded-lg block w-full p-2.5 focus:outline-none border-[#55c3c1f7] bg-transparent"
+            >
+              <option selected hidden>
+                Choose One
+              </option>
+              {countries.map((country) => (
+                <>
+                  {" "}
+                  <option value={country?.name?.common}>
+                    {country?.name?.common}
+                  </option>
+                </>
+              ))}
+            </select>
+          </div>
+
           <button
             type="submit"
-            className="bg-gradient-to-r from-[#031f4bee] to-[#55c3c1f7] py-2 px-7  text-white font-medium rounded-md mt-4 ml-auto block"
+            className="bg-[#55c3c1f7] py-2 px-7  text-white font-medium rounded-md mt-4 ml-auto block"
           >
             Save changes
           </button>
