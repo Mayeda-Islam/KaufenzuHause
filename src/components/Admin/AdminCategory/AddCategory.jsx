ab3fd5facff23dc0688a45e8e7a3f08054a9a8b6
unknown <mayedakonika@gmail.com>
2023-05-29 23:49:09 +0600

    scrollbar added

--- src/components/Admin/AdminCategory/AddCategory.jsx
+++ src/components/Admin/AdminCategory/AddCategory.jsx
@@ -11,6 +11,7 @@ const AddCategory = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
@@ -26,6 +27,7 @@ const AddCategory = () => {
       categoryTitle: data.categoryTitle,
       image: image,
     };
+    reset();
     fetch(`${serverUrl}/category`, {
       method: "POST",
       headers: {
@@ -43,67 +45,58 @@ const AddCategory = () => {
   }, []);
 
   return (
-    <div className="w-5/6 mx-auto lg:mt-12 ">
+    <div className="w-full lg:w-5/6 mx-auto lg:mt-12">
       <h1 className="text-2xl my-4 font-semibold">Add Category</h1>
-      <hr className="mb-12 " />
-      <form onSubmit={handleSubmit(onSubmit)} className=" mx-auto">
-        {/* register your input into the hook by invoking the "register" function */}
-        <div className="lg:flex Lg:justify-center lg:items-center lg:gap-4">
-          {" "}
-          <label className="text-lg lg:w-1/6  font-semibold lg:text-xl">
+      <hr className="mb-12" />
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full ">
+        <div className="flex flex-col items-start md:gap-4">
+          <label className="text-lg font-semibold md:text-xl">
             Image/SVG/Icons:
           </label>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full bprder">
             <input
               type="file"
               onChange={handleImage}
               accept="image/*"
               required
               multiple
-              // defaultValue={user?.email}
-
-              className="mt-2 w-full px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block  rounded-md sm:text-sm "
+              className="mt-2 w-full px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400 rounded-md sm:text-sm"
               placeholder="category title"
             />
             {errors?.categoryImage && (
-              <p className="text-red-500 text-sm ">
+              <p className="text-red-500 text-sm">
                 Category image/svg/icon is required
               </p>
             )}
           </div>
         </div>
 
-        <div className="lg:flex lg:justify-center lg:items-center lg:gap-4 my-4">
-          {" "}
-          <label className=" lg:w-1/6 font-semibold text-lg lg:text-xl">
+        <div className="flex flex-col items-start gap-4 my-4">
+          <label className="font-semibold text-lg md:text-xl">
             Category Title:
           </label>
           <div className="flex flex-col w-full">
             <input
               type="text"
               {...register("categoryTitle", { required: true })}
-              // defaultValue={user?.email}
-
-              className="mt-2  px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400  block w-full rounded-md sm:text-sm "
+              className="mt-2 px-3 py-2 border-2 shadow-sm focus:outline-none border-[#55c3c1f7] bg-transparent placeholder-slate-400 w-full rounded-md sm:text-sm"
               placeholder="your name"
             />
             {errors?.categoryTitle && (
-              <p className="text-red-500 text-sm ">
-                Category title is required
-              </p>
+              <p className="text-red-500 text-sm">Category title is required</p>
             )}
           </div>
         </div>
         <button
           type="submit"
-          className="bg-[#55c3c1f7] py-2 px-7  text-white font-medium rounded-md mt-4 ml-auto block"
+          className="bg-[#55c3c1f7] py-2 px-7 text-white font-medium rounded-md mt-4 ml-auto block"
         >
           Save
         </button>
       </form>
-      <div className="  mt-16">
+      <div className="mt-16 w-full sm:overflow-x-auto md:overflow-x-auto lg:overflow-x-auto xl:overflow-x-auto">
         <h1 className="text-2xl my-4 font-semibold">Category List</h1>
-        <hr className="mb-12 " />
+        <hr className="mb-12" />
         <CategoryList categories={categories}></CategoryList>
       </div>
     </div>
--- src/components/Admin/AdminCategory/CategoryList.jsx
+++ src/components/Admin/AdminCategory/CategoryList.jsx
@@ -2,41 +2,103 @@ import React from "react";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const CategoryList = ({ categories }) => {
+  const data = [
+    { id: 1, name: "John Doe", age: 25, email: "john@example.com" },
+    { id: 2, name: "Jane Smith", age: 30, email: "jane@example.com" },
+    // Add more data rows as needed
+  ];
+
   return (
-    <table className="border-collapse border relative overflow-x-auto border-gray-400 w-full">
-      <thead>
-        <tr>
-          <th className="border border-gray-400 py-4">Serial</th>
-          <th className="border border-gray-400 ">Category Image</th>
-          <th className="border border-gray-400">Category Title</th>
-          <th className="border border-gray-400">Action</th>
-          <th className="border border-gray-400">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {categories?.map((category, index) => (
-          <>
-            <tr className="text-center">
-              <td className="border border-gray-400">{index + 1}</td>
-              <td className="border border-gray-400 ">
+    <div className="overflow-x-auto">
+      <table className="border-collapse border border-gray-400 w-full">
+        <thead className="bg-gradient-to-r from-[#031f4bee] to-[#55c3c1f7] text-white font-bold text-lg">
+          <tr className="text-center text-white">
+            <th className="px-6 py-4  ">SL</th>
+
+            <th className="px-6 py-4  ">Category Image</th>
+
+            <th className="px-6 py-4  ">Category</th>
+
+            <th className="px-6 py-4  ">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories?.map((category, index) => (
+            <tr className="text-center " key={index}>
+              <td className="border border-gray-400 py-2 px-4 sm:px-6">
+                {index + 1}
+              </td>
+              <td className="border border-gray-400 py-2 px-4 sm:px-6">
                 <img
                   src={category.image}
                   className="w-10 h-10 rounded-full mx-auto"
                   alt=""
                 />
               </td>
-              <td className="border border-gray-400 text-md font-medium">
+              <td className="border border-gray-400 py-2 px-4 sm:px-6 text-md font-medium">
                 {category.categoryTitle}
               </td>
-              <td className="border border-gray-400  text-md ">Action</td>
-              <td className="border border-gray-400 text-red-400">
+
+              <td className="border border-gray-400 py-2 px-4 sm:px-6 text-red-400">
                 <DeleteForeverIcon />
               </td>
             </tr>
-          </>
-        ))}
-      </tbody>
-    </table>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+    // <div className="border-b border-gray-200 shadow  sm:overflow-x-auto md:overflow-x-auto lg:overflow-x-auto xl:overflow-x-auto ">
+    //   <table className="w-full table-auto min-w-full divide-y divide-gray-200 bg-[#F3F7FB] divide-y divide-gray-300">
+    //     <thead className=" bg-gradient-to-r from-[#6069d3ef] to-[#67f6f3c9] text-white font-bold text-lg">
+    //       <tr>
+    //         <th className="px-6 py-4 text-start text-white">SL</th>
+
+    //         <th className="px-6 py-4 text-start text-white">Title</th>
+
+    //         <th className="px-6 py-4 text-start text-white">Category</th>
+
+    //         <th className="px-6 py-4 text-start text-white">Action</th>
+    //       </tr>
+    //     </thead>
+    //     {/* <tbody className="bg-white divide-y divide-secondary">
+    //             {quizTotalData.map((group, i) => (
+    //               <AddQuizTable
+    //                 key={i}
+    //                 group={group}
+    //                 i={i}
+    //                 handleEditQuiz={handleEditQuiz}
+    //                 setQuizTotalData={setQuizTotalData}
+    //                 setAllQuiz={setAllQuiz}
+    //                 setShowEdit={setShowEdit}
+    //               ></AddQuizTable>
+    //             ))}
+    //           </tbody> */}
+    //   </table>
+    // </div>
+
+    // <div className="overflow-x-auto">
+    //   <table className="min-w-full divide-y divide-gray-200">
+    //     <thead className="bg-gray-50">
+    //       <tr>
+    //         <th className="px-4 py-2">ID</th>
+    //         <th className="px-4 py-2">Name</th>
+    //         <th className="px-4 py-2">Age</th>
+    //         <th className="px-4 py-2">Email</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody className="bg-white divide-y divide-gray-200">
+    //       {data.map((item) => (
+    //         <tr key={item.id}>
+    //           <td className="px-4 py-2">{item.id}</td>
+    //           <td className="px-4 py-2">{item.name}</td>
+    //           <td className="px-4 py-2">{item.age}</td>
+    //           <td className="px-4 py-2">{item.email}</td>
+    //         </tr>
+    //       ))}
+    //     </tbody>
+    //   </table>
+    // </div>
   );
 };
 
